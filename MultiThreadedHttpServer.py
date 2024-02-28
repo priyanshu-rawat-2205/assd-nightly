074a9d56f9d795ac3dc80334bba4d45ebdb71416
Priyanshu Rawat <dev.priyanshurawat@gmail.com>
2024-02-28 19:28:52 +0530

    initial commit

---
+++ MultiThreadedHttpServer.py
@@ -0,0 +1,34 @@
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler 
+import base64, json
+from entity import Entity
+class RequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        data = {}
+        response = ""
+        self.send_response(200)
+        self.send_header("content-type", "application/json")
+        self.end_headers()
+
+        if self.path == "/":
+            response = json.dumps({'status': "True"})
+
+        elif self.path == "/score":                     
+            data['center'] = Entity.center_coordinates
+            data['score'] = Entity.score
+
+            # file = open('shotTarget.jpg', 'rb')
+            # img = file.read()
+            # data['img'] = base64.b64encode(img).decode('utf-8')
+
+            response = json.dumps(data)
+
+        elif self.path == "/prepare":
+            Entity.flag = True  
+            response = json.dumps({'status': "True"})
+            
+
+        self.wfile.write(bytes(response, 'utf-8'))
+
+class Server(ThreadingHTTPServer):
+    pass
---
+++ entity.py
@@ -0,0 +1,4 @@
+class Entity:
+    flag = False
+    center_coordinates = None
+    score = None
